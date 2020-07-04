600bacd3ae71fb0cdf109fa33a72e14018c50aa3
Ben Mohammad <benmohammad593@gmail.com>
2020-07-04 19:37:56 +0100

    Initial commit

---
+++ app/src/main/java/com/benmohammad/orbitmvi/presentation/TodoActivity.kt
@@ -0,0 +1,13 @@
+package com.benmohammad.orbitmvi.presentation
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.benmohammad.orbitmvi.R
+
+class TodoActivity: AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
